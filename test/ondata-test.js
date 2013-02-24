29c23660f768c3b9a96833c8961ad5dfff9c835d
Roly Fentanes <roly426@gmail.com>
2013-02-24 04:17:23 -0700

    added more ondata tests

--- test/ondata-test.js
+++ test/ondata-test.js
@@ -1,49 +1,86 @@
-/*jshint quotmark:false, sub:true, es5:true */
+/*jshint quotmark:false, es5:true */
 var ondata = require('../lib/ondata');
 var EventEmitter = require('events').EventEmitter;
 
 
-exports['delete'] = function(test) {
-  var data = {
-    "delete":{
-      "status":{
-        "id":1234,
-        "id_str":"1234",
-        "user_id":3,
-        "user_id_str":"3"
+var data;
+var tests = {
+  delete: {
+    data: {
+      delete: {
+        status: data = {
+          id: 1234,
+          id_str: '1234',
+          user_id: 3,
+          user_id_str: '3'
+        }
       }
-    }
-  };
-
-  var ee = new EventEmitter();
-
-  ee.on('delete', function(tweetID, userID) {
-    test.equal(tweetID, data.delete.status.id_str);
-    test.equal(userID, data.delete.status.user_id_str);
-    test.done();
-  });
-
-  ondata.call(ee, data);
+    },
+    args: [data.id_str, data.user_id_str]
+  },
+  scrub_geo: {
+    data: {
+      scrub_geo: data = {
+        user_id: 14090452,
+        user_id_str: '14090452',
+        up_to_status_id: 23260136625,
+        up_to_status_id_str: '23260136625'
+      }
+    },
+    args: [data.user_id_str, data.up_to_status_id_str]
+  },
+  limit: {
+    data: { limit: { track: 1234 } },
+    args: [1234]
+  },
+  status_withheld: {
+    data: {
+      status_withheld: data = {
+        id: 1234567890,
+        id_str: '1234567890',
+        user_id: 123456,
+        user_id_str: '123456',
+        withheld_in_countries: ['DE', 'AR']
+      }
+    },
+    args: [data.id_str, data.user_id_str, data.withheld_in_countries]
+  },
+  user_withheld: {
+    data: {
+      user_withheld: data = {
+        id: 123456,
+        id_str: '123456',
+        withheld_in_countries: ['DE', 'AR']
+      }
+    },
+    args: [data.id_str, data.withheld_in_countries]
+  },
+  warning: {
+    data: {
+      warning: data = {
+        code: 'FALLING_BEHIND',
+        message: 'Your connection is falling behind and messages are being queued for delivery to you. Your queue is now over 60% full. You will be disconnected when the queue is full.',
+        percent_full: 60
+      }
+    },
+    args: [data.code, data.message, data.percent_full]
+  }
 };
 
 
-exports['scrub_geo'] = function(test) {
-  var data = {
-    "scrub_geo":{
-      "user_id":14090452,
-      "user_id_str":"14090452",
-      "up_to_status_id":23260136625,
-      "up_to_status_id_str":"23260136625"
-    }
+Object.keys(tests).forEach(function(event) {
+  var testData = tests[event];
+  exports[event] = function(test) {
+    var ee = new EventEmitter();
+
+    ee.on(event, function onEvent() {
+      for (var i = 0, l = testData.args.length; i < l; i++) {
+        test.deepEqual(testData.args[i], arguments[i]);
+      }
+
+      test.done();
+    });
+
+    ondata.call(ee, testData.data);
   };
-
-  var ee = new EventEmitter();
-
-  ee.on('scrub_geo', function(userID, upToStatusID) {
-    test.equal(userID, data.scrub_geo.user_id_str);
-    test.equal(upToStatusID, data.scrub_geo.up_to_status_id_str);
-    test.done();
-  });
-
-  ondata.call(ee, data);
-};
+});
